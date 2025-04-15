8e9439dd2bb24df4e770c1f22c7cd7a7d21ed8aa
moa-byggmastar <moabygg07@gmail.com>
2025-04-15 13:46:50 +0300

    added code

---
+++ components/Search.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useState } from 'react'
+
+export default function Search({ movies }) {
+    const [query, setQuery] = useState('')
+
+    const filteredMovies = movies.filter(movie =>
+        movie.title.toLowerCase().includes(query.toLowerCase())
+    )
+
+    return (
+        <div>
+            <input type="text" placeholder="Sök film..." value={query} onChange={(e) => setQuery(e.target.value)} />
+            <ul>
+                {filteredMovies.map(movie => (
+                    <li key={movie.id}>{movie.title}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
