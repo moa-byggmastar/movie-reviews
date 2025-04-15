7ceaefd440b9c8a84db2cab2f500eb3a884b96af
moa-byggmastar <moabygg07@gmail.com>
2025-04-15 13:55:43 +0300

    removed everything

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,22 +1,9 @@
 import React from 'react'
 
-export default async function MoviePage({ params }) {
-    const res = await fetch('http://localhost:3000/api/movies', {
-        cache: 'no-store'
-    })
-    const movies = await res.json()
-    const movie = movies.find(m => m.id === params.id)
-
-    if (!movie) {
-        return <h1>Filmen hittades inte.</h1>
-    }
-
+export default function MoviePage() {
     return (
         <div>
-            <h1>{movie.title}</h1>
-            <p>Recension: ★★★★☆</p>
-            <p>Mer info om filmen {movie.title}...</p>
-            <a href="/">Tillbaka</a>
+
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
-import Link from "next/link";
+import React from 'react'
 
-export default async function HomePage() {
-  const res = await fetch("http://localhost:3000/api/movies", {
-    cache: "no-store"
-  });
-  const movies = await res.json();
-
+export default function HomePage() {
   return (
     <div>
-      <h1>Filmrecensioner</h1>
-      <ul>
-        {movies.map(movie => (
-          <li key={movie.id}>
-            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        ))}
-      </ul>
+
     </div>
-  );
+  )
 }
