15135e4259a4ddadca203c5f0e500361b61c7f6d
moa-byggmastar <moabygg07@gmail.com>
2025-04-15 13:53:40 +0300

    tried to fix something

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 import Link from "next/link";
-import Search from "../components/Search";
 
 export default async function HomePage() {
   const res = await fetch("http://localhost:3000/api/movies", {
-    cache: "no-store" // för att hämta färska data varje gång
+    cache: "no-store"
   });
   const movies = await res.json();
 
   return (
     <div>
       <h1>Filmrecensioner</h1>
-
-      <Search movies={movies} />
-
       <ul>
         {movies.map(movie => (
           <li key={movie.id}>
--- components/Search.tsx
+++ components/Search.tsx
@@ -1,22 +1,27 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 export default function Search({ movies }) {
-    const [query, setQuery] = useState('')
+    const [query, setQuery] = useState("");
 
     const filteredMovies = movies.filter(movie =>
         movie.title.toLowerCase().includes(query.toLowerCase())
-    )
+    );
 
     return (
         <div>
-            <input type="text" placeholder="Sök film..." value={query} onChange={(e) => setQuery(e.target.value)} />
+            <input
+                type="text"
+                placeholder="Sök film..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
             <ul>
                 {filteredMovies.map(movie => (
                     <li key={movie.id}>{movie.title}</li>
                 ))}
             </ul>
         </div>
-    )
+    );
 }
